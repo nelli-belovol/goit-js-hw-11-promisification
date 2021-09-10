1d573e82a758488755320754d088208810e117f1
nelli-belovol <nelli199020@gmail.com>
2021-09-10 19:22:20 +0300

    fixed task-3

--- src/js/task-3.js
+++ src/js/task-3.js
@@ -10,23 +10,23 @@ const randomIntegerFromInterval = (min, max) => {
 const makeTransaction = transaction => {
   const delay = randomIntegerFromInterval(200, 500);
   const canProcess = Math.random() > 0.3;
-  const { id, amount } = transaction;
+
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       if (canProcess) {
-        resolve(`Transaction ${id} processed in ${amount}ms`);
+        resolve({ id: transaction.id, time: delay });
       }
-      reject(`Error processing transaction ${id}. Please try again later.`);
+      reject(transaction.id);
     });
   }, delay);
 };
 
-const logSuccess = success => {
-  console.log(success);
+const logSuccess = ({ id, time }) => {
+  console.log(`Transaction ${id} processed in ${time}ms`);
 };
 
-const logError = error => {
-  console.warn(error);
+const logError = id => {
+  console.warn(`Error processing transaction ${id}. Please try again later.`);
 };
 
 makeTransaction({ id: 70, amount: 150 }).then(logSuccess).catch(logError);
